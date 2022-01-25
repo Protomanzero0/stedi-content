a09c4e5c8249f5787af3df8bee62b648e6efd5bc
Protomanzero0 <protomanzero01@gmail.com>
2022-01-25 16:18:21 -0700

    Sends a text, no TfA

--- managecustomer.js
+++ managecustomer.js
@@ -104,4 +104,3 @@ function createcustomer(){
         dataType: 'text'
     });
 }
-
--- manageuser.js
+++ manageuser.js
@@ -51,7 +51,7 @@ function userlogin(){
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
+        url: 'https://dev.stedi.me/twofactorlogin/208-970-7665',
         data: JSON.stringify({userName, password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
